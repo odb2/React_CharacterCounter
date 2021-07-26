f58fc6457367d3e7963c6ed03f9c97327a63d835
Oscar <odbarragan99@gmail.com>
2021-07-25 22:43:46 -0700

    Updated features

--- src/TextEntry.js
+++ src/TextEntry.js
@@ -10,29 +10,42 @@ class TextEntry extends Component {
             CharacterArray: []
         }
     }
+    //Function which runs when text is changed in textbox
     textchange(e) {
-        var array = e.target.value.split('');
-        var obj = {};
+        //Converts input text to an array filled with individual characters
+        //Converts input text to lowercase
+        var input_text = e.target.value.toLowerCase();
+        var array = input_text.split('');
+        
+        //Removes whitespace elements in array
+        array = array.filter(function(str) {
+            return /\S/.test(str);
+        });
 
+        //Creates object which contains the individual characters and 
+        //the amount of times they appear
+        var obj = {};
         for (var i=0; i < array.length; i++) {
             obj[array[i]] = (obj[array[i]] || 0) + 1;
         }
 
-        //Copys the obj which contains the counts of characters
-        var copy = {};
-        Object.assign(copy,obj);
-
+        //manipulates the input string to shrink to only unique characters
+        //the reason for this was because feeding obj to render 
+        //always made them in the wrong order
         var uniq = [...new Set(array)];
         var uniq2 = [...new Set(array)];
         uniq2.reverse();
 
+        //Copys the obj which contains the counts of characters
+        var copy = {};
+        Object.assign(copy,obj);
+        //Sorts the characters from smallest to largest
         var keysSorted = uniq2.sort(function(a,b){
             return copy[a]-copy[b]
         })
-        console.log(keysSorted)
 
         this.setState({
-            CharacterCount: e.target.value.length,
+            CharacterCount: array.length,
             CharactersDict: obj,
             CharactersTopFive_keys: keysSorted.slice(-5),
             CharacterArray: uniq
@@ -48,15 +61,15 @@ class TextEntry extends Component {
                 <p>Total Number of characters: {this.state.CharacterCount}</p>
                 <table>
                     <tr>
-                    <th>Character</th>
-                    <th>Total Amount</th>
+                        <th>Character</th>
+                        <th>Total Amount</th>
                     </tr>
                     {
-                        this.state.CharacterArray.map( key =>
-                        <tr id={key} style={{ backgroundColor: (this.state.CharactersTopFive_keys.includes(key)) ? "yellow" : "white" }}>
-                            <td>{key}</td>
-                            <td>{this.state.CharactersDict[key]}</td>
-                        </tr>
+                    this.state.CharacterArray.map( key =>
+                    <tr id={key} style={{ backgroundColor: (this.state.CharactersTopFive_keys.includes(key)) ? "yellow" : "white" }}>
+                        <td>{key}</td>
+                        <td>{this.state.CharactersDict[key]}</td>
+                    </tr>
                     )}
                 </table>
             </div>
